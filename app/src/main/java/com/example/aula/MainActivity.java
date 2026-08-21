3caa3bb27bcbb9a18e2135e131a17cb427611b54
braian <braianhenriquemoreira@gmail.com>
2026-08-21 12:09:51 -0300

    comitao pra poxima aula

--- app/src/main/java/com/example/aula/MainActivity.java
+++ app/src/main/java/com/example/aula/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.aula;
 
 import android.os.Bundle;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
@@ -9,6 +10,8 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+
+import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
     int contador = 0;
@@ -23,13 +26,17 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
-
-
+        EditText edmin = findViewById(R.id.edmin);
+        EditText edmax = findViewById(R.id.edmax);
         Button butao= findViewById(R.id.button);
         TextView tv = findViewById(R.id.textView);
+
+
         butao.setOnClickListener(view -> {
-            contador = (int)(Math.random() * 100) + 1;
-            tv.setText(Integer.toString(contador));
+            int min = Integer.parseInt(edmin.getText().toString());
+            Random random = new Random();
+            int valor= random.nextInt(min,max);
+            tv.setText(Integer.toString(valor));
         });
 
     }
